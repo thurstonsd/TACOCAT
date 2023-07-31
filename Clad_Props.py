571c4d5a5d61c34e02bf49696e41fa5395a5d3c4
thurstonsd <133273949+thurstonsd@users.noreply.github.com>
2023-07-31 14:22:51 -0400

    Delete Clad_Props.py
    Mistaken Upload

--- Clad_Props.py
+++
@@ -1,8 +0,0 @@
-import Clad_Props as Clinput
-Cladding_Type = Clinput.Fuel_props
-print('----------------------------------------------')
-print('Kanth_AMPT:')
-print('Zirc_2:')
-print('Stain_Steel_304:')
-print('Zirc_2_Nickel_Free:')
-print('-----------------------------------------------')
